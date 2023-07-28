c3f224cc22d182587dcafe1c453624e129161898
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-07-28 05:07:40 -0400

    Add js show/hide div functionality upon submit click

---
+++ main.js
@@ -0,0 +1,6 @@
+function thankYou() {
+    let thankYouDisplay = document.getElementById('thank-you-container');
+    let ratingDisplay = document.getElementById('rating-state-container');
+    thankYouDisplay.style.display = "block";
+    ratingDisplay.style.display = "none";
+}
