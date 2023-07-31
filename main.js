fd00dc254c026b1cc34cc797004777614afeab48
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-07-31 15:42:15 -0400

    Update rating selection logic for confirmation page

--- main.js
+++ main.js
@@ -5,65 +5,12 @@ function thankYou() {
     ratingDisplay.style.display = "none";
 }
 
-let numArray = document.querySelectorAll('.rating-num');
+const ratingButtons = document.querySelectorAll('.rating-num');
+const selectedRatingElement = document.getElementById('rating-num-selection');
 
-document.getElementById('rating-num-selection').innerHTML 
-
-// console.log(numArray[0].value)
-
-function getRatingSelection() {
-    let ratingNum = 0;
-    let numList = document.querySelectorAll('.rating-num');
-    // let numArray = [...numList];
-    let NumSelection = document.getElementById('rating-num-selection').innerHTML;
-    for (let i = 0; i < numList.length; i++) {
-        ratingNum = numList[i].value
-        console.log(ratingNum)
-        if (ratingNum === 1) {
-            console.log(ratingNum.value)
-            return NumSelection = "1"
-        } else if (ratingNum === 2) {
-            document.getElementById('rating-num-selection').innerHTML = "2"
-        } 
-    }
-    
-    return
-}
-
-// console.log(getRatingSelection())
-
-// function ratingSelection() {
-//     let ratingNum = document.getElementById('rating-num').innerHTML;
-//     let ratingNumDisplay = document.getElementById('rating-num-selection').innerHTML
-//     ratingNumDisplay = ratingNum
-//     return ratingNumDisplay
-//     }
-
-// function getRatingNum() {
-//     let ratingNum = document.getElementsByName('rating').innerText;
-//     for (let i = 0; i < ratingNum.length; i++) {
-//         if (ratingNum[i] === 1) {
-//             document.getElementById('rating-num-selection').innerText = "1";
-//         } else {
-//             return "No selection made"
-//         }   
-//     }
-// }
-
-// let ratingNum1 = document.getElementById('num1').innerHTML;
-// document.getElementById('rating-num-selection').innerHTML = ratingNum1;
-
-// let ratingNum2 = document.getElementById('num2').innerHTML;
-// document.getElementById('rating-num-selection').innerHTML = ratingNum2;
-
-// let ratingNum3 = document.getElementById('num2').innerHTML;
-// document.getElementById('rating-num-selection').innerHTML = ratingNum3;
-
-// let ratingNum4 = document.getElementById('num2').innerHTML;
-// document.getElementById('rating-num-selection').innerHTML = ratingNum4;
-
-// let ratingNum5 = document.getElementById('num2').innerHTML;
-// document.getElementById('rating-num-selection').innerHTML = ratingNum;
-
-// let ratingNum = document.querySelectorAll('rating-num').innerHTML;
-// document.querySelectorAll('rating-num-selection').innerHTML = ratingNum;
+ratingButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const ratingValue = button.textContent;
+        selectedRatingElement.textContent = ratingValue
+    })
+})
